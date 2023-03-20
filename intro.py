b6d2d501ad6571a7e3e4e8c72b03bd56bc3fbcd0
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-20 23:34:22 +0100

    completed second part of the project

--- intro.py
+++ intro.py
@@ -1,2 +1,7 @@
+# first part of the project
 print("Hello! My name is PotatoHelper3000")
 print("I was created in 2023")
+# second part of the project
+print('Please, remind me your name.')
+user_input = input()
+print('What a great name you have, ' + user_input + "!")
