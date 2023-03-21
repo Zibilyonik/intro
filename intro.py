5d1c4578d5257dbd8235a49e9657b83baba3051a
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-21 15:05:29 +0100

    completed fourth part of the project

--- intro.py
+++ intro.py
@@ -13,3 +13,11 @@ remainder5 = int(input())
 remainder7 = int(input())
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print("Your age is " + str(age) + "; that's a good time to start programming!")
+# fourth part of the project
+print('Now I will prove to you that I can count to any number you want.')
+number = int(input())
+count = 0
+while count <= number:
+    print(str(count) + " !")
+    count += 1
+print('Completed, have a nice day!')
