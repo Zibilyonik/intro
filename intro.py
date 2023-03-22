f76b32f0344ad2c318da554637a5560775cd9fb9
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-22 11:51:18 +0100

    completed the last step of the project

--- intro.py
+++ intro.py
@@ -1,23 +1,60 @@
-# first part of the project
-print("Hello! My name is PotatoHelper3000")
-print("I was created in 2023")
-# second part of the project
-print('Please, remind me your name.')
-user_input = input()
-print('What a great name you have, ' + user_input + "!")
-# third part of the project
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
-remainder3 = int(input())
-remainder5 = int(input())
-remainder7 = int(input())
-age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-print("Your age is " + str(age) + "; that's a good time to start programming!")
-# fourth part of the project
-print('Now I will prove to you that I can count to any number you want.')
-number = int(input())
-count = 0
-while count <= number:
-    print(str(count) + " !")
-    count += 1
-print('Completed, have a nice day!')
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print("How many potatoes do you need to change a lightbulb?")
+    print("1. 1")
+    print("2. 2")
+    print("3. 3")
+    print("4. 4")
+    correct = False
+    while correct == False:
+        answer = input()
+        if int(answer) != 2:
+            print("Please, try again.")
+        else:
+            correct = True
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('PotatoHelper3000', '2023')
+remind_name()
+guess_age()
+count()
+test()
+end()
